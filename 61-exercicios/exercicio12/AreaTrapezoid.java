246ea55c097a0ea857702c820a140ef5554cb09c
LuisD <luis.domiciano@hotmail.com>
2021-01-31 23:07:02 -0300

    feat: solution exercicio 12

---
+++ 61-exercicios/exercicio12/AreaTrapezoid.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class AreaTrapezoid {
+
+    public static void main (String [] args) {
+        
+        int longBase = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a base maior:"));
+        int shortBase = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a base menor:"));
+        int altitude = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a altura:"));
+        int area = area(longBase, shortBase, altitude);
+
+        JOptionPane.showMessageDialog(null, "Area do Trapezio: " + area);
+
+    }
+    static int area (int longBase, int shortBase, int altitude) {
+        return ( (longBase + shortBase) /2)* altitude;
+    }
+}
---
+++ 61-exercicios/exercicio12/README.md
@@ -0,0 +1,4 @@
+# Exercício 12
+Escreva um programa que calcule a área do Trapézio.
+## Fórmula 
+area do trapezio = ((base maior + base menor) / 2) x altura
