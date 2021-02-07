5f044a9c5b99fe6843f864b035547a230fbbcd4d
LuisD <luis.domiciano@hotmail.com>
2021-02-07 15:01:44 -0300

    feat: solution 14

---
+++ 61-exercicios/exercicio14/Exercicio14.java
@@ -0,0 +1,9 @@
+public class Exercicio14 {
+  public static void main (String []args) {
+    int start = 32, end = 126;
+
+    for ( int character = start; character <= end; character++ ) {
+      System.out.printf("%d =  %c\n", character, character);
+    }
+  }
+}
---
+++ 61-exercicios/exercicio14/README.md
@@ -0,0 +1,2 @@
+# Exercício 14
+Escreva um programa que exiba a posição e o respectivo caractere entre as posições 32 e 126 da tabela ascii.
